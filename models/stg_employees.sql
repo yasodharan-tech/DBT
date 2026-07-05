28efa28d66f2e278721eef14be1eaef008816eec
yaso dharan <yasomarine24@gmail.com>
2026-07-05 12:51:06 +0000

    no

---
+++ models/stg_employees.sql
@@ -0,0 +1,14 @@
+select 
+    EMPLOYEE_ID,
+    FIRST_NAME,
+    LAST_NAME,
+    EMAIL,
+    PHONE_NUMBER,
+    HIRE_DATE,
+    JOB_ID,
+    SALARY,
+    COMMISSION_PCT,
+    MANAGER_ID,
+    DEPARTMENT_ID,
+current_timestamp from hr.src_employees
+where SALARY >=10000
